a83bcd4920ef7d311967ebaf558936aca5466b7a
Shoter99 <shoter998@gmail.com>
2021-05-10 19:35:26 +0200

    Added desc

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,12 @@ const auth = firebase.auth();
 
 const whenSignIn = document.getElementById('LogedIn')
 const whenSignOut = document.getElementById('LogedOut')
+const descSection = document.getElementById('description')
 
 const signInBtn = document.getElementById('signInBtn')
 const signOutBtn = document.getElementById('signOutBtn')
 const anSignInBtn = document.getElementById('anSignIn')
+const descBtn = document.getElementById('descBtn')
 
 const usernameInput = document.getElementById('username')
 
@@ -15,6 +17,14 @@ const provider = new firebase.auth.GoogleAuthProvider()
 signInBtn.onclick = () => auth.signInWithPopup(provider)
 signOutBtn.onclick = () => auth.signOut()
 anSignInBtn.onclick = () => auth.signInAnonymously()
+descBtn.onclick = () => {
+    console.log(descSection.isHidden)
+    if(descSection.hidden){
+        descSection.hidden = false
+    }else{
+        descSection.hidden = true
+    }
+}
 
 firebase.auth().onAuthStateChanged(user => {
     if(user){
