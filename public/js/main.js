77b828acbc95713c6b3a2dbdcb7415880d13a3ec
Shoter99 <shoter998@gmail.com>
2021-05-10 08:13:45 +0200

    Started auth

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,7 @@
-console.log(firebase)
+const auth = firebase.auth();
+
+const whenSignIn = document.getElementById('LogedIn')
+const whenSignOut = document.getElementById('LogedOut')
+
+const signInBtn = document.getElementById('signInBtn')
+const signOutBtn = document.getElementById('signOutBtn')
