70e1129e34e5c5a35c07c14d9c57ede6db149dfb
Shoter99 <shoter998@gmail.com>
2021-05-10 18:52:22 +0200

    Added anonymous sign in

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,31 @@
 const auth = firebase.auth();
 
+
 const whenSignIn = document.getElementById('LogedIn')
 const whenSignOut = document.getElementById('LogedOut')
 
 const signInBtn = document.getElementById('signInBtn')
 const signOutBtn = document.getElementById('signOutBtn')
+const anSignInBtn = document.getElementById('anSignIn')
+
+const usernameInput = document.getElementById('username')
 
 const provider = new firebase.auth.GoogleAuthProvider()
 
 signInBtn.onclick = () => auth.signInWithPopup(provider)
+signOutBtn.onclick = () => auth.signOut()
+anSignInBtn.onclick = () => auth.signInAnonymously()
 
-signOutBtn = () => auth.signOut()
+firebase.auth().onAuthStateChanged(user => {
+    if(user){
+        // User is signed in
+        whenSignIn.hidden = false
+        whenSignOut.hidden = true
+        username.value = user.displayName
+    }
+    else{
+        // No user is sign in
+        whenSignIn.hidden = true
+        whenSignOut.hidden = false
+    }
+})
